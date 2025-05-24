d76709833409759a273457c13a8d3fcbfdfb7c19
SugaSensei <sugatraj.sarwade@gmail.com>
2025-05-24 23:36:41 +0530

    Fix formatting issue by removing trailing whitespace in audio file output line

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from moviepy.editor import VideoFileClip
 video_path = "videos/sample.mp4"
 
 video = VideoFileClip(video_path)
-video.audio.write_audiofile("output/sample_audio.wav    ")
+video.audio.write_audiofile("output/sample_audio.wav")
